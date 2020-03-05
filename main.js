eae32d71e4ffe086cc01b7f6426d3feab23b52b6
Colin Jaffe <balloonasaurus@gmail.com>
2020-03-04 22:20:26 -0500

    Finishes basic solution.

---
+++ main.js
@@ -0,0 +1,49 @@
+const firstListItems = document.querySelectorAll('ol li');
+
+const addStrikethrough = function(event) {
+  event.target.style.textDecoration = 'line-through';
+}
+
+for (const listItem of firstListItems) {
+  listItem.addEventListener('click', addStrikethrough);
+}
+
+const secondListItems = document.querySelectorAll('ul li');
+
+const fade = function(event) {
+  event.target.style.opacity = 0;
+}
+
+for (const listItem of secondListItems) {
+  listItem.addEventListener('click', fade);
+}
+
+const pictures = document.querySelectorAll('#row img');
+
+const collapse = function(event) {
+  event.target.style.width = '0px';
+}
+
+for (const picture of pictures) {
+  picture.addEventListener('click', collapse);
+}
+
+
+const meteor = document.querySelector('#toggle');
+
+const destroyEverything = function() {
+  for (const listItem of firstListItems) {
+    listItem.style.textDecoration = 'line-through';
+  }
+
+  for (const listItem of secondListItems) {
+    listItem.style.opacity = 0;
+  }
+
+  for (const picture of pictures) {
+    picture.style.width = '0px';
+  }
+}
+
+
+meteor.addEventListener('click', destroyEverything);
